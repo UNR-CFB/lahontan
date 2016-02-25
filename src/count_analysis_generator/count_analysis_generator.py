6c6c3315f47c79ba42fc524a60541734c2224720
Sebastian Smith <taypworm@gmail.com>
2016-02-24 20:59:17 -0800

    working count_analysis_generator

--- src/count_analysis_generator/count_analysis_generator.py
+++ src/count_analysis_generator/count_analysis_generator.py
@@ -4,6 +4,33 @@ import csv
 import os
 from optparse import OptionParser
 
+class GeneCounts(object):
+	def __init__(self):
+		self.header = [ 'GeneID', 'Length' ]
+		self.sample_names = []
+		self.gene_lengths = {}
+		self.gene_counts = {}
+
+	def add_counts(self, sample_name, geneID, length, counts):
+		if sample_name not in self.sample_names:
+			self.sample_names.append(sample_name)
+
+		if geneID not in self.gene_lengths:
+			self.gene_lengths[geneID] = length
+
+		if geneID in self.gene_counts:
+			self.gene_counts[geneID][sample_name] = counts
+		else:
+			self.gene_counts[geneID] = { sample_name: counts }
+
+	def get_header(self):
+		return self.header + self.sample_names
+
+	def __iter__(self):
+		for k, v in self.gene_counts.iteritems():
+			yield [k, self.gene_lengths[k]] + [v[sample] for sample in self.sample_names]
+
+
 def main():
 	parser = OptionParser()
 	parser.add_option('-f', '--in', dest='in_files', type='string', action='callback', callback=multi_file_callback, help='input files, separate multiple files with comma')
@@ -11,64 +38,41 @@ def main():
 
 	option, args = parser.parse_args()
 
-	aggregate_gene_counts = {}
+	counts = GeneCounts()
 	for file_name in option.in_files:
 		# use filename as header
-		header = os.path.splitext(os.path.basename(file_name))[0]
-		gene_counts = get_gene_counts(file_name)
-		for k, v in gene_counts.iteritems():
-			if k in aggregate_gene_counts:
-				aggregate_gene_counts[k].gene_counts[header] = v
-			else:
-				summary = GeneSummary()
-				summary.length = v['length']
-				summary.gene_counts = { header: v['counts'] }
-				aggregate_gene_counts[k] = summary
+		sample = os.path.splitext(os.path.basename(file_name))[0]
 
-	print aggregate_gene_counts
+		for count in read_gene_counts(file_name):
+			counts.add_counts(sample, count['geneid'], count['length'], count['counts'])
 
-def get_gene_counts(filename):
-	gene_counts = {}
+	write_gene_counts(option.out_file, counts)
+
+def read_gene_counts(filename):
 	with open(filename) as f:
 		# throw away first line, contains program invocation
+		f.readline()
+		# throw away second line, contains header
 		f.readline()
 
 		# read counts file
 		reader = csv.reader(f, delimiter='\t')
 		for row in reader:
-			gene_counts[row[0]] = { 'length': row[5],
-				'counts': row[6] }
+			yield { 'geneid': row[0], 'length': int(row[5]), 'counts': int(row[6]) }
 
-	return gene_counts
+def write_gene_counts(filename, genes):
+	with open(filename, 'wb') as f:
+		writer = csv.writer(f, delimiter='\t')
+
+		# write header
+		writer.writerow(genes.get_header())
+
+		# write data
+		for data in genes:
+			writer.writerow(data)
 
 def multi_file_callback(option, opt, value, parser):
 	setattr(parser.values, option.dest, value.split(','))
-
-
-class GeneCounts(object):
-	def __init__(self):
-		self.header = [ 'GeneID', 'Length' ]
-		self.sample_names = []
-		self.gene_length = {}
-		self.gene_counts = {}
-
-	def add_counts(sample_name, geneID, length, counts):
-		if sample_name not in self.sample_names:
-			self.sample_names.append(sample_name)
-
-		if gene not in self.gene_length:
-			self.gene_length[gene] = length
-
-		self.sample_counts.setdefault(gene, { sample_name: counts }).append(counts)
-
-	def __str__(self):
-		pass
-
-class GeneSummary(object):
-	def __init__(self):
-		self.length = 0
-		self.gene_counts = {}
-
 
 if __name__ == '__main__':
 	main()
