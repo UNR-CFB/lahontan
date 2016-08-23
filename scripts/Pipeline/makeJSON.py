3277bf97ecb9b147f24da5ac9ab97d683d1259a7
Alberto Nava <anava@nevada.unr.edu>
2016-08-23 22:43:47 +0000

    Added script to make Metadata

---
+++ scripts/Pipeline/makeJSON.py
@@ -0,0 +1,49 @@
+'''Usage: makeJSON.py [-h | --help] [-f <file>]
+
+Options:
+    -h --help           Show this screen
+    -f <file>           Optional name of JSON file to be saved to [default: Metadata.json] 
+'''
+from docopt import docopt
+import json
+
+def getProjectInfo():
+    projectName = str(input("What is the name of the project? "))
+    numberofSamples = int(input("How many samples are there? "))
+    numberofFeatures = int(input("How many features does each sample have? "))
+    return projectName,numberofSamples,numberofFeatures
+
+def getSampleInfo():
+    projectName,numberofSamples,numberofFeatures = getProjectInfo()
+    SampleData = {}
+
+    for sample in range(numberofSamples):
+        SampleData['sample_%.2d'%(sample+1)] = {}
+        SampleData['sample_%.2d'%(sample+1)]['Features'] = []
+        for feature in range(numberofFeatures):
+            feat = str(input('What is feature#%d for sample#%d? '%(feature+1,sample+1)))
+            SampleData['sample_%.2d'%(sample+1)]['Features'].append(feat)
+    return projectName,numberofSamples,numberofFeatures,SampleData
+
+def makeJSON():
+    MetaData = {}
+
+    projectName,numberofSamples,numberofFeatures,sampleData = getSampleInfo()
+
+    MetaData['ProjectName'] = projectName
+    MetaData['NumberofSamples'] = numberofSamples
+    MetaData['NumberofFeatures'] = numberofFeatures
+    MetaData['Samples'] = sampleData
+
+    return MetaData
+
+def writeJSON(name):
+    MetaDict = makeJSON()
+
+    with open(name,'w') as File:
+        json.dump(MetaDict, File, sort_keys=True,indent=4)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__,version='1.0')
+    print(arguments)
+    writeJSON(arguments['-f'])
