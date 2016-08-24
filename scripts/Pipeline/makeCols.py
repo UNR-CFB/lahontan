2c4323503e6ff68ec1f8bc0ced5c4536d127f170
Alberto Nava <anava@nevada.unr.edu>
2016-08-24 18:17:57 +0000

    developed an actual Cols.dat from JSON data

---
+++ scripts/Pipeline/makeCols.py
@@ -0,0 +1,46 @@
+'''Usage: readJSON.py [-h | --help] [-f <file>] [-t <tofile>]
+
+Options:
+    -h --help           Show this screen
+    -f <file>           Optional name of JSON file to be read [default: Metadata.json] 
+    -t <tofile>         Optional name of Column file to write to [default: Cols.dat]
+'''
+from docopt import docopt
+from pprint import pprint
+import json
+
+def readJSON(name):
+    with open(name) as jsonFile:
+        jsonData = json.load(jsonFile)
+    return jsonData
+
+def parseJSON(jsontoRead):
+    projectName = jsontoRead['ProjectName']
+    numberofFeatures = jsontoRead['NumberofFeatures']
+    numberofSamples = jsontoRead['NumberofSamples']
+    featureNames = jsontoRead['FeatureNames']
+    return featureNames,projectName,numberofFeatures,numberofSamples
+
+def parseSamples(jsontoRead):
+    SampleList = []
+    counter = 0
+    for sample in jsontoRead['Samples']:
+        SampleList.append([sample])
+        for feature in jsontoRead['Samples'][sample]['Features']:
+            SampleList[counter].append(jsontoRead['Samples'][sample]['Features'][feature])
+        counter += 1
+    return SampleList
+
+def makeCols(jsontoRead,filetoWrite):
+    featureNames,_,__,___ = parseJSON(jsontoRead)
+    sampleList = parseSamples(jsontoRead)
+    deseqCols = []
+    deseqCols.append(featureNames)
+    for sample in sampleList:
+        deseqCols.append(sample)
+    with open(filetoWrite,'w') as colFile:
+        colFile.writelines('\t'.join(row) + '\n' for row in deseqCols)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__,version='1.0')
+    makeCols(readJSON(arguments['-f']),arguments['-t'])
