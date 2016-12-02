851dcf83ba177e0ae43bf737e37f5fd048d416ff
Alberto <anava@nevada.unr.edu>
2016-12-02 19:42:37 +0000

    Updated trimmomatic blacklist

--- scripts/Pipeline/makeTrimBlacklist.py
+++ scripts/Pipeline/makeTrimBlacklist.py
@@ -8,6 +8,14 @@ def blacklist():
 TACACTCTTTCCCTACACGACGCTCTTCCGATCT
 >PrefixPE/2
 GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT
+>PE1
+TACACTCTTTCCCTACACGACGCTCTTCCGATCT
+>PE1_rc
+AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA
+>PE2
+GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT
+>PE2_rc
+AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
 >ABISolid3AdapterB
 CCTATCCCCTGTGTGCCTTGGCAGTCTCAGCCTCTCTATGGGCAGTCGGT
 >PCR_Primer1
@@ -25,17 +33,30 @@ TTTTTTTTTTCAAGCAGAAGACGGCATACGA
 {A}
 {T}
 {C}
-{G}'''
+{G}
+{A2}
+{T2}
+{C2}
+{G2}
+'''
 
-    As = '>ConsecutiveA\n'+'A'*500
-    Ts = '>ConsecutiveT\n'+'T'*500
-    Cs = '>ConsecutiveC\n'+'C'*500
-    Gs = '>ConsecutiveG\n'+'G'*500
+    As = '>ConsecutiveA\n'+'A'*50
+    Ts = '>ConsecutiveT\n'+'T'*50
+    Cs = '>ConsecutiveC\n'+'C'*50
+    Gs = '>ConsecutiveG\n'+'G'*50
+    As2 = '>ConsecutiveAb\n'+'A'*30
+    Ts2 = '>ConsecutiveTb\n'+'T'*30
+    Cs2 = '>ConsecutiveCb\n'+'C'*30
+    Gs2 = '>ConsecutiveGb\n'+'G'*30
     Context = {
             'A': As,
             'T': Ts,
             'C': Cs,
-            'G': Gs
+            'G': Gs,
+            'A2': As2,
+            'T2': Ts2,
+            'C2': Cs2,
+            'G2': Gs2
             }
     goodBlacklist = Page.format(**Context)
     with open('TruSeq3-PE.fa','w') as F:
